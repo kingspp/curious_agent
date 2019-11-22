260c64c80d4f672e10d5b5c5f8947a7229f8edfc
Mahdi Alouane <malouane@wpi.edu>
2019-11-22 16:49:09 -0500

    [WIP] Added dependency fixes
    - OSX dependency fix
    - Added CWD to PYTHONPATH

--- samples/breakout_dqn.py
+++ samples/breakout_dqn.py
@@ -1,8 +1,16 @@
+
+import sys
+import os
+
+# Add the project root to the PYTHONPATH env variable
+sys.path.insert(0, os.getcwd())
+# OSX depdency conflict resolution for ffmpeg and OMP
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 from curious_agent.pipelines import Pipeline
 from curious_agent.agents.open_ai_agents.dqn_agent import DQNAgent
 from curious_agent.environments.open_ai.atari.atari_environment import AtariEnvironment
 from curious_agent.util import pipeline_config_loader
-import sys
 
 config = pipeline_config_loader(sys.argv[1])
 
