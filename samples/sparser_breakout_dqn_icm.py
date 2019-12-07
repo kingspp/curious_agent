2b9fb15001f89887a9de6094c2f225f2af93619a
kingspp <kingspprathyush@gmail.com>
2019-12-06 23:36:30 -0500

    Added sparser dqn agent with icm support

---
+++ samples/sparser_breakout_dqn_icm.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@created on: 12/6/19,
+@author: Prathyush SP,
+@version: v0.0.1
+@system name: kingspp
+Description:
+
+..todo::
+
+"""
+
+import sys
+import os
+import torch
+
+# Add the project root to the PYTHONPATH env variable
+sys.path.insert(0, os.getcwd())
+# OSX depdency conflict resolution for ffmpeg and OMP
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+from curious_agent.pipelines import Pipeline
+from curious_agent.agents.open_ai_agents.sparse_dqn_agent_icm import SparseDQNAgentICM
+from curious_agent.environments.open_ai.atari.atari_environment import AtariEnvironment
+from curious_agent.util import pipeline_config_loader
+
+# Load Configuration
+config = pipeline_config_loader(sys.argv[1])
+
+torch.set_default_tensor_type('torch.cuda.FloatTensor' if config['agent_config'].device == "cuda" else 'torch.FloatTensor')
+
+# Create Environment
+env = AtariEnvironment(config['env_config'], atari_wrapper=True)
+# Create Agent
+agent = SparseDQNAgentICM(env, config['agent_config'])
+
+# Load the environment and agent into the pipeline
+pipeline = Pipeline(train_agent=agent, environment=env, config=config, test=True)
+
+# Run the training loop
+pipeline.execute()
+# pipeline._load_pipeline()
+# pipeline.resume()
